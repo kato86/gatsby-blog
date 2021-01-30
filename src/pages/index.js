21be2366ec950245bc13363dbb2c9038e7c0d048
Kamil <tomala.kamil@hotmail.com>
2021-01-30 21:49:42 +0100

    Styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import style from 'styled-components';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
+
+const StyledH1 = style.h1`
+    color: rebeccapurple;
+`;
 
 export default () => {
     const { title, description } = useSiteMetadata();
     return (
         <>
-        <h1>{ title }</h1>
+        <StyledH1>{ title }</StyledH1>
         <p>{ description }</p>
         </>
     )
