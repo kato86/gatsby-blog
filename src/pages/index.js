4cdf5b5fb37049f6430cd76b355db18fd6b238c2
Kamil <tomala.kamil@hotmail.com>
2021-01-30 22:13:25 +0100

    Adding layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import style from 'styled-components';
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
-
-const StyledH1 = style.h1`
-    color: rebeccapurple;
-`;
+import { Layout } from '../components/Layout';
 
 export default () => {
-    const { title, description } = useSiteMetadata();
     return (
         <>
-        <StyledH1>{ title }</StyledH1>
-        <p>{ description }</p>
+            <Layout />
         </>
-    )
-}
+    );
+};
