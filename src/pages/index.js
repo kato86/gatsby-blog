2f65bb6c581a3a01ebb8a9d82d4fefa2b6dee793
Kamil <tomala.kamil@hotmail.com>
2021-02-02 22:12:35 +0100

    Index page post query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,40 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import { Layout } from '../components/Layout';
 
-export default () => {
+export default ({ data }) => {
     return (
         <>
-            <Layout />
+            <Layout>
+                {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+                    <>
+                        <h1>{frontmatter.title}</h1>
+                        <p>{frontmatter.data}</p>
+                        <p>{excerpt}</p>
+                    </>
+                ))}
+            </Layout>
         </>
     );
 };
+
+export const query = graphql`
+    query SITE_INDEX_QUERY {
+        allMdx(
+            sort: {fields: frontmatter___date, order: DESC} 
+            filter: {frontmatter: {published: {eq: true}}}
+        ) {
+            nodes {
+                id
+                excerpt(pruneLength: 250)
+                frontmatter {
+                    title
+                    date
+                }
+            }
+        }
+
+    }
+`;
+
+// NEXT --> Slug and Paths
